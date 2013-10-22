ddd7c5eab0814fdedbcea99bb5e8f56b0ed7e768
Stanley <tehapple@ubuntu.(none)>
2013-10-22 17:59:46 -0400

    added g7 player and fruitGenerator

---
+++ fruit/g7/FruitGenerator.java
@@ -0,0 +1,17 @@
+package fruit.dumb;
+
+public class FruitGenerator implements fruit.sim.FruitGenerator
+{
+    public int[] generate(int nplayers, int bowlsize) {
+        int nfruits = nplayers * bowlsize;
+
+        int[] dist = new int[12];
+        int unit = nfruits / 12;
+        
+        dist[0] = nfruits - unit * 11;
+        for (int i = 1; i < 12; i++)
+            dist[i] = unit;
+        
+        return dist;
+    }
+}
