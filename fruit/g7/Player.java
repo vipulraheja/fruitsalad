67f01cb11c36c6a93cf74aae346a4ba76441f2e1
Vipul Raheja <vipulraheja@gmail.com>
2013-10-26 03:10:24 -0400

    History Added

--- fruit/g7/Player.java
+++ fruit/g7/Player.java
@@ -2,21 +2,38 @@ package fruit.g7;
 
 import java.util.*;
 
-
 public class Player extends fruit.sim.Player
 {
     int[] preference;
+    int n_players;
+    int n_bowls;
+    private int[][] fruitHistory;
 
+    // Since there are 12 Players
+    private static int NUM_FRUITS = 12;
+   
     public void init(int nplayers, int[] pref) {
 	    preference = pref;
-    }
+	    n_players = nplayers;
 
+	    // Since there are 2 rounds
+	    n_bowls = nplayers*2;
+
+	    fruitHistory = new int[n_bowls][NUM_FRUITS];
+	    for (int i=0 ; i<n_bowls ; i++){
+		    for (int j=0 ; j<NUM_FRUITS ; j++){
+			    fruitHistory[i][j] = 0;
+		    }
+	    }
+    }
+    
     public boolean pass(int[] bowl, int bowlId, int round,
                         boolean canPick,
                         boolean musTake) {
 	double expected = expectedValue(bowl, bowlId, round);
-
-	System.out.println("Pref:\n");
+	
+	fruitHistory = history(bowl, bowlId, round);
+	
 	for (int i=0 ; i<preference.length ; i++)
 		System.out.print(preference[i] + ", ");
 
@@ -28,11 +45,18 @@ public class Player extends fruit.sim.Player
 
         return false;
     }
+
+    private int[][] history(int[] bowl, int bowlId, int round){
+	    for (int i=0 ; i<bowl.length ; i++){
+		    fruitHistory[round*n_players + bowlId][i] = bowl[i];
+	    }
     
+	    return fruitHistory;
+    }
 
     private Random random = new Random();
 
-    public int score(int[] bowl, int bowlId, int round){
+    private int score(int[] bowl, int bowlId, int round){
 	    int score = 0;
 	    for (int i=0 ; i<bowl.length ; i++){
 		    score += preference[i] * bowl[i];
@@ -41,7 +65,7 @@ public class Player extends fruit.sim.Player
 	    return score;
     }
 
-    public double expectedValue(int[] bowl, int bowlId, int round){
+    private double expectedValue(int[] bowl, int bowlId, int round){
 	    
 	    int bowlSize = 0;
 	    for(int i=0; i<bowl.length; i++)
