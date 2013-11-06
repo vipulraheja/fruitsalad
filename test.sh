c0272957b55cf8e6ea7628b8302a62ed10af4057
tehapple <stanley0856@gmail.com>
2013-11-06 01:12:23 -0500

    added test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+for i in 2 3 5 10 15 30 100;
+do
+	java fruit.sim.Fruit players.list $i dist/RandomizedDistribution1.txt false false 5000 2> r$i
+	java fruit.sim.Fruit players.list $i dist/uniform.txt false false 5000 2> u$i
+done
+
+for i in 2 3 5 10 15 30 100;
+do
+	echo "Random1 $i" >> result.txt
+	tail -17 r$i  >> result.txt
+	echo "" >> result.txt
+	rm r$i
+
+	echo "uniform $i" >> result.txt
+	tail -17 u$i  >> result.txt
+	echo "" >> result.txt
+	rm u$i
+done
