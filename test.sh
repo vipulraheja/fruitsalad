d34b67cb2ad7fb5d2456df5cc627b8f8f36644ad
tehapple <stanley0856@gmail.com>
2013-11-06 01:31:38 -0500

    modified test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 for i in 2 3 5 10 15 30 100;
 do
-	java fruit.sim.Fruit players.list $i dist/RandomizedDistribution1.txt false false 5000 2> r$i
-	java fruit.sim.Fruit players.list $i dist/uniform.txt false false 5000 2> u$i
+	java fruit.sim.Fruit players.list $i dist/RandomizedDistribution1.txt false false 1000 2> r$i
+	java fruit.sim.Fruit players.list $i dist/uniform.txt false false 1000 2> u$i
 done
 
 for i in 2 3 5 10 15 30 100;
